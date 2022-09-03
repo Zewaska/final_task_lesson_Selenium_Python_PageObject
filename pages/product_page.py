aeaa70d5fed934c5f84aa49f5dace3455c2a597b
Zewaska <pikalovyaros@gmail.com>
2022-09-03 17:25:56 +0300

    in base add two methods, add locator BasePage, add test login link

--- pages/product_page.py
+++ pages/product_page.py
@@ -22,3 +22,9 @@ class ProductPage(BasePage):
         
     def should_be_dress_price(self):
         assert self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text == self.browser.find_element(*ProductPageLocators.BOOK_PRICE_ALERT).text, "Another Price Name"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Message not is disappeared, but should be"
