f871d5b25a27b1a5d058222735a1c38af4efa98f
Mihail <89089193+Zewaska@users.noreply.github.com>
2022-09-04 12:50:46 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,8 +12,7 @@ class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
-
+        
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
